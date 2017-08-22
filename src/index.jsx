b74c83f091666b72ed2bd9ac1ba7060f64bb5de8
Rohan Almeida <almeida@aarohan.biz>
2017-08-23 01:31:15 +0530

    react

---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+
+// Entry point of app
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/*
+  import { createStore } from 'redux';
+  import { Provider } from 'react-redux';
+  import App from './containers/App';
+  import reducers from './reducers';
+
+  let store = createStore(reducers);
+
+  ReactDOM.render(
+  <Provider store={store}>
+    <div className="app">
+      <h3>Container App (contains redux logic)</h3>
+      <App />
+    </div>
+  </Provider>,
+  document.getElementById('root')
+  );
+*/
+
+ReactDOM.render(
+    <h3>Flask with React</h3>,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+
+module.exports = {
+  entry:  {
+    app: __dirname + '/src/index.jsx',
+  },
+
+  output: {
+    path:     __dirname + '/www/builds',
+    filename: 'bundle.js',
+		publicPath: "/builds/",
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: __dirname + '/src',
+      },
+        /*
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        include: __dirname + '/src',
+      },
+      */
+    ],
+  },
+
+  devtool: 'source-map',
+};
+
