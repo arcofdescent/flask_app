e6bd25749532a1c215dee00a22b91b36df800048
Rohan Almeida <almeida@aarohan.biz>
2017-08-23 20:55:14 +0530

    /stock/quote

--- app.py
+++ app.py
@@ -4,32 +4,32 @@ import sys
 from flask import Flask, render_template, request, make_response
 from pprint import pprint
 import json
-import time
+import requests
+import logging
 
 app = Flask(__name__)
 
+iex_endpoint = 'https://api.iextrading.com/1.0'
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    ret = { 'status': 'ok' }
+    res = make_response(json.dumps(ret))
+    res.headers['Content-Type'] = 'application/json'
+    
+    return res
 
-"""
-@app.route('/get_shortest_route', methods=['POST'])
-def get_shortest_route():
+@app.route('/stock/<symbol>/quote')
+def get_quote(symbol):
 
-    post_data = request.data.decode("utf-8")
-    points = json.loads(post_data)
-    points = [tsp.Point(p['id'], p['x'], p['y']) for p in points['points']]
-
-    start_time = time.time()
-    distances = tsp.calc_distance_between_points(points)
-    route_info = tsp.calc_shortest_route(len(points), distances)
-    route_info['duration'] = "%.4f" % (time.time() - start_time)
-
-    res = make_response(json.dumps(route_info))
-    res.headers['Access-Control-Allow-Origin'] = "*"
+    iex_url = iex_endpoint + "/stock/{}/quote".format(symbol)
+    r = requests.get(iex_url)
+    
+    res = make_response(r.text)
+    res.headers['Content-Type'] = 'application/json'
+    #res.headers['Access-Control-Allow-Origin'] = "*"
 
     return res
-"""
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
